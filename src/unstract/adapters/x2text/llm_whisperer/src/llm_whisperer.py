84adb078a289d8625277a8090ae34274b77cb384
Chandrasekharan M <chandrasekharan@zipstack.com>
2024-03-10 18:41:14 +0530

    UN-906 Support for new LLMWhisperer service, bumped adapters to 0.4.1

--- src/unstract/adapters/x2text/llm_whisperer/src/llm_whisperer.py
+++ src/unstract/adapters/x2text/llm_whisperer/src/llm_whisperer.py
@@ -1,29 +1,25 @@
 import logging
 import os
 from typing import Any, Optional
-from urllib.parse import urlencode
 
 import requests
 from requests import Response
+from requests.exceptions import RequestException
 
 from unstract.adapters.exceptions import AdapterError
 from unstract.adapters.x2text.constants import X2TextConstants
 from unstract.adapters.x2text.llm_whisperer.src.constants import (
-    OCRFilters,
+    HTTPMethod,
+    OCRDefaults,
     OutputModes,
     ProcessingModes,
+    WhispererConfig,
+    WhispererEndpoint,
+    WhispererHeader,
 )
 from unstract.adapters.x2text.x2text_adapter import X2TextAdapter
 
 logger = logging.getLogger(__name__)
-
-
-class Constants:
-    URL = "url"
-    TEST_CONNECTION = "test-connection"
-    WHISPER = "whisper"
-    PROCESSING_MODE = "processing_mode"
-    OUTPUT_MODE = "output_mode"
 
 
 class LLMWhisperer(X2TextAdapter):
@@ -59,14 +55,15 @@ class LLMWhisperer(X2TextAdapter):
 
     def _make_request(
         self,
-        request_type: str,
+        request_verb: HTTPMethod,
+        request_endpoint: str,
+        params: Optional[dict[str, Any]] = None,
         files: Optional[dict[Any, Any]] = None,
-        **kwargs: Optional[dict[Any, Any]],
     ) -> Response:
         llm_whisperer_svc_url = (
-            f"{self.config.get(Constants.URL)}"
-            f"/api/v1/llm-whisperer/{request_type}"
+            f"{self.config.get(WhispererConfig.URL)}" f"/v1/{request_endpoint}"
         )
+        # Required when whisperer service is run locally
         platform_service_api_key = self.config.get(
             X2TextConstants.PLATFORM_SERVICE_API_KEY
         )
@@ -74,43 +71,43 @@ class LLMWhisperer(X2TextAdapter):
         headers = {
             "accept": "application/json",
             "Authorization": f"Bearer {platform_service_api_key}",
+            WhispererHeader.UNSTRACT_KEY: self.config.get(
+                WhispererConfig.UNSTRACT_KEY
+            ),
         }
 
-        # Add files only if the request is for whisper
         data = None
-        params = None
-        if request_type == Constants.WHISPER and files is not None:
+        if files is not None:
             f = files["file"]
             data = f.read()
             headers["Content-Type"] = "application/octet-stream"
 
-            processing_mode_value = self.config.get(
-                Constants.PROCESSING_MODE, ProcessingModes.TEXT.value
-            )
-            if processing_mode_value == ProcessingModes.OCR.value:
-                processing_mode_value = (
-                    f"{processing_mode_value}?median_filter_size="
-                    f"{OCRFilters.MEDIAN_FILTER_SIZE.value}"
-                    f"&gaussian_blur_radius="
-                    f"{OCRFilters.GAUSSIAN_BLUR_RADIUS.value}"
+        try:
+            if request_verb == HTTPMethod.GET:
+                response = requests.get(
+                    url=llm_whisperer_svc_url, headers=headers  # type: ignore
                 )
-
-            params = {
-                Constants.PROCESSING_MODE: processing_mode_value,
-                Constants.OUTPUT_MODE: self.config.get(
-                    Constants.OUTPUT_MODE, OutputModes.LINE_PRINTER.value
-                ),
-            }
-
-        query_string = urlencode(params) if params else ""
-        llm_whisperer_svc_url += f"?{query_string}" if query_string else ""
-
-        response = requests.post(
-            llm_whisperer_svc_url, headers=headers, data=data
-        )
+            elif request_verb == HTTPMethod.POST:
+                response = requests.post(
+                    url=llm_whisperer_svc_url,
+                    headers=headers,  # type: ignore
+                    params=params,
+                    data=data,
+                )
+            response.raise_for_status()
+        except RequestException as e:
+            err_response: Response = e.response  # type: ignore
+            if err_response.headers["Content-Type"] == "application/json":
+                err_json = err_response.json()
+                if "message" in err_json:
+                    raise AdapterError(err_json["message"])
+            elif err_response.headers["Content-Type"] == "text/plain":
+                raise AdapterError(err_response.text())  # type: ignore
+            else:
+                raise AdapterError
         return response
 
-    def whisper(
+    def process(
         self,
         input_file_path: str,
         output_file_path: Optional[str] = None,
@@ -119,53 +116,63 @@ class LLMWhisperer(X2TextAdapter):
         try:
             input_f = open(input_file_path, "rb")
             files = {"file": input_f}
+
+            params = {
+                WhispererConfig.PROCESSING_MODE: self.config.get(
+                    WhispererConfig.PROCESSING_MODE, ProcessingModes.TEXT.value
+                ),
+                WhispererConfig.OUTPUT_MODE: self.config.get(
+                    WhispererConfig.OUTPUT_MODE, OutputModes.LINE_PRINTER.value
+                ),
+            }
+            if (
+                params[WhispererConfig.PROCESSING_MODE]
+                == ProcessingModes.OCR.value
+            ):
+                params.update(
+                    {
+                        WhispererConfig.MEDIAN_FILTER_SIZE: self.config.get(
+                            WhispererConfig.MEDIAN_FILTER_SIZE,
+                            OCRDefaults.MEDIAN_FILTER_SIZE,
+                        ),
+                        WhispererConfig.GAUSSIAN_BLUR_RADIUS: self.config.get(
+                            WhispererConfig.GAUSSIAN_BLUR_RADIUS,
+                            OCRDefaults.GAUSSIAN_BLUR_RADIUS,
+                        ),
+                    }
+                )
+
             response = self._make_request(
-                request_type=Constants.WHISPER, files=files, **kwargs
+                request_verb=HTTPMethod.POST,
+                request_endpoint=WhispererEndpoint.WHISPER,
+                params=params,
+                files=files,
             )
-            if response.status_code != 200:
-                logger.error(
-                    "Error in LLM Whisperer whisper document: "
-                    f"[{response.status_code}] {response.reason}"
-                )
-                raise AdapterError(
-                    f"{response.status_code} - {response.reason}"
-                )
-            else:
-                if response.content is not None:
-                    if isinstance(response.content, bytes):
-                        output = response.content.decode("utf-8")
-                    if output_file_path is not None:
-                        with open(output_file_path, "w", encoding="utf-8") as f:
-                            f.write(output)
-                            f.close()
-                    return output
+            if response.ok and response.content is not None:
+                if isinstance(response.content, bytes):
+                    output = response.content.decode("utf-8")
+                if output_file_path is not None:
+                    with open(output_file_path, "w", encoding="utf-8") as f:
+                        f.write(output)
+                        f.close()
+                return output
+            return ""
+        except AdapterError:
+            raise
+        # TODO: Review this practice and remove if unnecessary
         except Exception as e:
-            logger.error(f"Error occured while processing document {e}")
-            if not isinstance(e, AdapterError):
-                raise AdapterError(str(e))
-            else:
-                raise e
+            logger.error(f"Error occured while processing document: {e}")
+            raise AdapterError(str(e))
         finally:
             if input_f is not None:
                 input_f.close()
 
     def test_connection(self) -> bool:
         try:
-            response = self._make_request(Constants.TEST_CONNECTION)
-            if response.status_code != 200:
-                logger.error(
-                    "Error in LLM Whisperer test-connection: "
-                    f"[{response.status_code}] {response.reason}"
-                )
-
-                raise AdapterError(
-                    f"{response.status_code} - {response.reason}"
-                )
-            else:
-                return True
-        except Exception as e:
-            logger.error(f"Error occured while testing adapter {e}")
-            if not isinstance(e, AdapterError):
-                raise AdapterError(str(e))
-            else:
-                raise e
+            self._make_request(
+                request_verb=HTTPMethod.GET,
+                request_endpoint=WhispererEndpoint.TEST_CONNECTION,
+            )
+        except AdapterError:
+            raise
+        return True
