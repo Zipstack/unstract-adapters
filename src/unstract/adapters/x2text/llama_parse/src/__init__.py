e9a3e5cb968ad301fe98b1deaa43d8478d29d038
harini-venkataraman <harini@zipstack.com>
2024-04-22 19:43:48 +0530

    Integrate Llama parse X2Text Adapter

---
+++ src/unstract/adapters/x2text/llama_parse/src/__init__.py
@@ -0,0 +1,9 @@
+from llama_parse import LlamaParse
+
+metadata = {
+    "name": LlamaParse.__name__,
+    "version": "1.0.0",
+    "adapter": LlamaParse,
+    "description": "LlamaParse X2Text adapter",
+    "is_active": True,
+}
