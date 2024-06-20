9531997694091ed41ab4b44d3359470c0556695f
harini-venkataraman <harini@zipstack.com>
2024-06-20 16:07:59 +0530

    Encrypting postrges connection URL

--- src/unstract/adapters/vectordb/postgres/src/postgres.py
+++ src/unstract/adapters/vectordb/postgres/src/postgres.py
@@ -1,4 +1,5 @@
 import os
+import urllib.parse
 from typing import Any, Optional
 
 import psycopg2
@@ -58,6 +59,9 @@ class Postgres(VectorDBAdapter):
 
     def _get_vector_db_instance(self) -> BasePydanticVectorStore:
         try:
+            encrypted_password = urllib.parse.quote_plus(
+                str(self._config.get(Constants.PASSWORD))
+            )
             self._collection_name = VectorDBHelper.get_collection_name(
                 self._config.get(VectorDbConstants.VECTOR_DB_NAME),
                 self._config.get(VectorDbConstants.EMBEDDING_DIMENSION),
@@ -74,7 +78,7 @@ class Postgres(VectorDBAdapter):
                 database=self._config.get(Constants.DATABASE),
                 schema_name=self._schema_name,
                 host=self._config.get(Constants.HOST),
-                password=self._config.get(Constants.PASSWORD),
+                password=encrypted_password,
                 port=str(self._config.get(Constants.PORT)),
                 user=self._config.get(Constants.USER),
                 table_name=self._collection_name,
