82c2af6db95813f099853582eef18b3e3c06e48e
Gayathri <142381512+gaya3-zipstack@users.noreply.github.com>
2024-06-21 14:13:50 +0530

    simplify None check
    Co-authored-by: Deepak K <89829542+Deepak-Kesavan@users.noreply.github.com>
    Signed-off-by: Gayathri <142381512+gaya3-zipstack@users.noreply.github.com>

--- src/unstract/adapters/vectordb/helper.py
+++ src/unstract/adapters/vectordb/helper.py
@@ -97,7 +97,7 @@ class VectorDBHelper:
             vector_db_collection_name = (
                 vector_db_collection_name + "_" + str(embedding_dimension)
             )
-        if collection_name_prefix is not None:
+        if collection_name_prefix:
             vector_db_collection_name = (
                 collection_name_prefix + vector_db_collection_name
             )
